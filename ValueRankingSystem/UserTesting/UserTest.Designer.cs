fdc1b5050a35c285698757857646450725655d8b
Kevin Khlom <kevin.khlom@pcc.edu>
2018-10-16 18:40:35 -0700

    Initial commit of what has been done

---
+++ ValueRankingSystem/UserTesting/UserTest.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace UserTesting
+{
+    partial class UserTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userChoiceOne = new MetroFramework.Controls.MetroRadioButton();
+            this.userChoiceTwo = new MetroFramework.Controls.MetroRadioButton();
+            this.userChoiceThree = new MetroFramework.Controls.MetroRadioButton();
+            this.testButton = new MetroFramework.Controls.MetroButton();
+            this.directionLabel = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // userChoiceOne
+            // 
+            this.userChoiceOne.AutoSize = true;
+            this.userChoiceOne.Location = new System.Drawing.Point(116, 263);
+            this.userChoiceOne.Name = "userChoiceOne";
+            this.userChoiceOne.Size = new System.Drawing.Size(65, 17);
+            this.userChoiceOne.TabIndex = 0;
+            this.userChoiceOne.Text = "demo1";
+            this.userChoiceOne.UseSelectable = true;
+            // 
+            // userChoiceTwo
+            // 
+            this.userChoiceTwo.AutoSize = true;
+            this.userChoiceTwo.Location = new System.Drawing.Point(320, 263);
+            this.userChoiceTwo.Name = "userChoiceTwo";
+            this.userChoiceTwo.Size = new System.Drawing.Size(65, 17);
+            this.userChoiceTwo.TabIndex = 1;
+            this.userChoiceTwo.Text = "demo2";
+            this.userChoiceTwo.UseSelectable = true;
+            // 
+            // userChoiceThree
+            // 
+            this.userChoiceThree.AutoSize = true;
+            this.userChoiceThree.Location = new System.Drawing.Point(524, 263);
+            this.userChoiceThree.Name = "userChoiceThree";
+            this.userChoiceThree.Size = new System.Drawing.Size(124, 17);
+            this.userChoiceThree.TabIndex = 2;
+            this.userChoiceThree.Text = "demoCantDecide";
+            this.userChoiceThree.UseSelectable = true;
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(285, 339);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(226, 66);
+            this.testButton.TabIndex = 3;
+            this.testButton.Text = "&Next";
+            this.testButton.UseSelectable = true;
+            this.testButton.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // directionLabel
+            // 
+            this.directionLabel.AutoSize = true;
+            this.directionLabel.Location = new System.Drawing.Point(227, 123);
+            this.directionLabel.Name = "directionLabel";
+            this.directionLabel.Size = new System.Drawing.Size(322, 20);
+            this.directionLabel.TabIndex = 4;
+            this.directionLabel.Text = "From the choices below, which one do you prefer?";
+            // 
+            // UserTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.directionLabel);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.userChoiceThree);
+            this.Controls.Add(this.userChoiceTwo);
+            this.Controls.Add(this.userChoiceOne);
+            this.Name = "UserTest";
+            this.Text = "User Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroRadioButton userChoiceOne;
+        private MetroFramework.Controls.MetroRadioButton userChoiceTwo;
+        private MetroFramework.Controls.MetroRadioButton userChoiceThree;
+        private MetroFramework.Controls.MetroButton testButton;
+        private MetroFramework.Controls.MetroLabel directionLabel;
+    }
+}
+
---
+++ ValueRankingSystem/UserTesting/UserTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace UserTesting
+{
+    public partial class UserTest : MetroForm
+    {
+        public UserTest()
+        {
+            InitializeComponent();
+        }
+        // Test Variables
+        public string firstItem;
+        public string secondItem;
+        public string thirdItem;
+        public string userName;
+        public bool stringExist;
+        Object[] TestTakerInfo = new object[4];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Sample Data
+            firstItem = "Blue";
+            secondItem = "Green";
+            thirdItem = "Can't Decide";
+            userName = "Demo User";
+            populateObjectArray();
+            populateRadio();
+            checkArray(TestTakerInfo);
+        }
+        // Assigns all variables to a definition in order to be called when the user logins
+        private void populateObjectArray()
+        {
+            TestTakerInfo[0] = userName;
+            TestTakerInfo[1] = firstItem;
+            TestTakerInfo[2] = secondItem;
+            TestTakerInfo[3] = thirdItem;
+        }
+        // Changes the radio buttons based content in array
+        private void populateRadio()
+        {
+            userChoiceOne.Text = TestTakerInfo[1].ToString();
+            userChoiceTwo.Text = TestTakerInfo[2].ToString();
+            userChoiceThree.Text = TestTakerInfo[3].ToString();
+        }
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Looks to see if there are any available tests
+                // Need to ask the instructor if the same test can be reused
+                if (userChoiceOne.Checked)
+                {
+                    MessageBox.Show("You chose " + TestTakerInfo[1].ToString());
+                    //Need to add a function to write to the database
+                }
+                else if (userChoiceTwo.Checked)
+                {
+                    MessageBox.Show("You chose " + TestTakerInfo[2].ToString());
+                }
+                else if (userChoiceThree.Checked)
+                {
+                    MessageBox.Show("You chose " + TestTakerInfo[3].ToString());
+                }
+                else
+                {
+                    MessageBox.Show("Please make a choice");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong with the display");
+            }
+        }
+        private void addChoiceToDb()
+        {
+            // Push choice to DB after the user clicks on finish
+        }
+        // Check to see if the array has any values
+        public static void checkArray(Object[] TestTakerInfo)
+        {
+            bool stringExist = true;
+            int counter = 0;
+            foreach (var stringItem in TestTakerInfo)
+            {
+                counter++;
+                if (TestTakerInfo[counter-1] == null)
+                {
+                    stringExist = false;
+                }
+            }
+            if (stringExist == false)
+            {
+                MessageBox.Show("Unable to load test and user data");
+            }
+        }
+    }
+}
